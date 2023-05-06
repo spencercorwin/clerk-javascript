210ac1400063d2ff6d2f9d20e8f419096876e43c
Spencer Corwin <spencercorwin@icloud.com>
2023-05-05 21:24:43 -0700

    feat(backend): Create types for each webhook event

--- packages/backend/src/api/resources/Webhooks.ts
+++ packages/backend/src/api/resources/Webhooks.ts
@@ -9,28 +9,62 @@ import type {
 
 type Webhook<EvtType, Data> = { type: EvtType; object: 'event'; data: Data };
 
-export type UserWebhookEvent =
-  | Webhook<'user.created' | 'user.updated', UserJSON>
-  | Webhook<'user.deleted', DeletedObjectJSON>;
+export type UserCreatedWebhookEvent = Webhook<'user.created', UserJSON>;
+export type UserUpdatedWebhookEvent = Webhook<'user.updated', UserJSON>;
+export type UserDeletedWebhookEvent = Webhook<'user.deleted', DeletedObjectJSON>;
+export type UserWebhookEvent = UserCreatedWebhookEvent | UserUpdatedWebhookEvent | UserDeletedWebhookEvent;
 
-export type SessionWebhookEvent = Webhook<
-  'session.created' | 'session.ended' | 'session.removed' | 'session.revoked',
-  SessionJSON
->;
+export type SessionCreatedWebhookEvent = Webhook<'session.created', SessionJSON>;
+export type SessionEndedWebhookEvent = Webhook<'session.ended', SessionJSON>;
+export type SessionRemovedWebhookEvent = Webhook<'session.removed', SessionJSON>;
+export type SessionRevokedWebhookEvent = Webhook<'session.revoked', SessionJSON>;
+export type SessionWebhookEvent =
+  | SessionCreatedWebhookEvent
+  | SessionEndedWebhookEvent
+  | SessionRemovedWebhookEvent
+  | SessionRevokedWebhookEvent;
 
+export type OrganizationCreatedWebhookEvent = Webhook<'organization.created', OrganizationJSON>;
+export type OrganizationUpdatedWebhookEvent = Webhook<'organization.updated', OrganizationJSON>;
+export type OrganizationDeletedWebhookEvent = Webhook<'organization.deleted', DeletedObjectJSON>;
 export type OrganizationWebhookEvent =
-  | Webhook<'organization.created' | 'organization.updated', OrganizationJSON>
-  | Webhook<'organization.deleted', DeletedObjectJSON>;
+  | OrganizationCreatedWebhookEvent
+  | OrganizationUpdatedWebhookEvent
+  | OrganizationDeletedWebhookEvent;
 
-export type OrganizationMembershipWebhookEvent = Webhook<
-  'organizationMembership.created' | 'organizationMembership.accepted' | 'organizationMembership.revoked',
+export type OrganizationMembershipCreatedWebhookEvent = Webhook<
+  'organizationMembership.created',
   OrganizationMembershipJSON
 >;
+export type OrganizationMembershipAcceptedWebhookEvent = Webhook<
+  'organizationMembership.accepted',
+  OrganizationMembershipJSON
+>;
+export type OrganizationMembershipRevokedWebhookEvent = Webhook<
+  'organizationMembership.revoked',
+  OrganizationMembershipJSON
+>;
+export type OrganizationMembershipWebhookEvent =
+  | OrganizationMembershipCreatedWebhookEvent
+  | OrganizationMembershipAcceptedWebhookEvent
+  | OrganizationMembershipRevokedWebhookEvent;
 
-export type OrganizationInvitationWebhookEvent = Webhook<
-  'organizationInvitation.created' | 'organizationInvitation.deleted' | 'organizationInvitation.updated',
+export type OrganizationInvitationCreatedWebhookEvent = Webhook<
+  'organizationInvitation.created',
   OrganizationInvitationJSON
 >;
+export type OrganizationInvitationDeletedWebhookEvent = Webhook<
+  'organizationInvitation.deleted',
+  OrganizationInvitationJSON
+>;
+export type OrganizationInvitationUpdatedWebhookEvent = Webhook<
+  'organizationInvitation.updated',
+  OrganizationInvitationJSON
+>;
+export type OrganizationInvitationWebhookEvent =
+  | OrganizationInvitationCreatedWebhookEvent
+  | OrganizationInvitationDeletedWebhookEvent
+  | OrganizationInvitationUpdatedWebhookEvent;
 
 export type WebhookEvent =
   | UserWebhookEvent
